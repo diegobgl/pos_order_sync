40f6b259c8cfd768ddc212e88c8ca3556e238136
Diego Gajardo <diegobgajardo@gmail.com>
2024-01-20 01:18:31 -0300

    5

--- __manifest__.py
+++ __manifest__.py
@@ -23,7 +23,7 @@
         'security/ir.model.access.csv',
         'views/pos_order_view.xml',
         #'views/pos_config_view.xml',
-        #'views/templates.xml',
+        'views/templates.xml',
         'security/groups.xml'
     ],
     'demo': [],
@@ -36,6 +36,7 @@
     'web.assets_qweb': [
             'pos_order_sync/static/src/xml/**/*',
         ],
+    }
 
 
 
